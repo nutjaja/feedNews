391c1dd115d815fbe0be6a2ef17f0fe8b0bc1e76
suriya <suriya@gmail.com>
2018-05-03 22:54:15 +0700

    head

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <center><h1>จัดการหัวข้อข่าว / เพิ่มรายการข่าว </h1> </center>  <br>
+    <input type="text" v-model="code"> <br>
+    <input type="text" v-model="name">
+    <button @click="addstudent">ADD</button>
   </div>
 </template>
 <script>
