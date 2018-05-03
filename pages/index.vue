089b14524142a8ec1eab1c96be484b732332382b
suriya <suriya@gmail.com>
2018-05-03 22:58:06 +0700

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <center><h1>จัดการหัวข้อข่าว / เพิ่มรายการข่าว </h1> </center>  <br>
-    <input type="text" v-model="code"> <br>
-    <input type="text" v-model="name">
+  
     <button @click="addNews">ADD</button> 
     <button @click="CANCLE">CANCLE</button>
   </div>
