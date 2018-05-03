5f67504c7377d61e2e0bcea6e6e8ee991047c889
suriya <suriya@gmail.com>
2018-05-03 23:01:28 +0700

    test subheader

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
   <div>
     <center><h1>จัดการหัวข้อข่าว / เพิ่มรายการข่าว </h1> </center>  <br>
-  
-    <button @click="addNews">ADD</button> 
-    <button @click="CANCLE">CANCLE</button>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Normal with hint text/label</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-1"
+          label="Label Text"
+          id="testing"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Focus</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-2"
+          label="Label Text"
+          value="Input text"
+          class="input-group--focused"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Normal with input text + label</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-3"
+          label="Label Text"
+          value="Input text"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Disabled</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-3"
+          label="Label Text"
+          value="Input text"
+          disabled
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container> 
   </div>
 </template>
 <script>
